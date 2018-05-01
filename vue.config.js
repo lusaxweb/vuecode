5e448320323f387f454f76e2126b03bae16d2f3b
luis daniel <luisdanielrovira8@gmail.com>
2018-05-01 13:22:07 -0400

    change name

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,8 @@
-
-module.exports = {}
+var path = require('path')
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'vuecode.js'
+  },
+}
